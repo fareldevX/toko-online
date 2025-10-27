60003a0fe161128bc6d2b78dc3f83ad8fc0242e1
Farel Arlish Orlando <mobillamborghini9999@gmail.com>
2025-10-27 13:45:24 +0700

    Update website (new section: About Our)

--- src/js/script.js
+++ src/js/script.js
@@ -3,20 +3,42 @@ const btn = document.getElementById("btn");
 const isMobile = document.getElementById("isMobile");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    navbar.classList.add("fixed");
-    navbar.classList.remove("absolute");
-    navbar.classList.add("backdrop-blur-md", "bg-white/30", "shadow-md");
-    navbar.classList.remove("bg-transparent");
+  const scrollY = window.scrollY;
+
+  if (scrollY > 30) {
+    navbar.classList.add("-translate-y-10", "opacity-0");
+  }
+
+  if (scrollY > 50) {
+    navbar.classList.remove("-translate-y-10", "opacity-0");
+    navbar.classList.add(
+      "-translate-y-0",
+      "opacity-100",
+      "fixed",
+      "backdrop-blur-md",
+      "bg-white/30",
+      "shadow-md"
+    );
+    navbar.classList.remove("absolute", "bg-transparent");
   } else {
     if (!isMobile.classList.contains("hidden")) {
       return isMobile.classList.add("hidden");
     }
 
-    navbar.classList.remove("fixed");
-    navbar.classList.add("absolute");
-    navbar.classList.remove("backdrop-blur-md", "bg-white/30", "shadow-md");
-    navbar.classList.add("bg-transparent");
+    navbar.classList.remove(
+      "fixed",
+      "-translate-y-10",
+      "opacity-0",
+      "backdrop-blur-md",
+      "bg-white/30",
+      "shadow-md"
+    );
+    navbar.classList.add(
+      "-translate-y-0",
+      "opacity-100",
+      "absolute",
+      "bg-transparent"
+    );
   }
 });
 
@@ -24,27 +46,29 @@ btn.addEventListener("click", () => {
   const isHidden = isMobile.classList.contains("hidden");
 
   if (isHidden) {
-    isMobile.classList.remove("hidden");
-    setTimeout(() => {
-      isMobile.classList.remove("-translate-y-5", "opacity-0");
-      isMobile.classList.add("-translate-y-0", "opacity-100");
-    }, 10);
+    showMobileMenu();
   } else {
-    isMobile.classList.remove("-translate-y-0", "opacity-100");
-    isMobile.classList.add("-translate-y-5", "opacity-0");
-    setTimeout(() => {
-      isMobile.classList.add("hidden");
-    }, 300);
+    hideMobileMenu();
   }
 
   const links = isMobile.querySelectorAll("a");
   links.forEach((link) => {
-    link.addEventListener("click", () => {
-      isMobile.classList.remove("-translate-y-0", "opacity-100");
-      isMobile.classList.add("-translate-y-5", "opacity-0");
-      setTimeout(() => {
-        isMobile.classList.add("hidden");
-      }, 300);
-    });
+    link.addEventListener("click", () => hideMobileMenu);
   });
 });
+
+const showMobileMenu = () => {
+  isMobile.classList.remove("hidden");
+  setTimeout(() => {
+    isMobile.classList.remove("-translate-y-5", "opacity-0");
+    isMobile.classList.add("-translate-y-0", "opacity-100");
+  }, 10);
+};
+
+const hideMobileMenu = () => {
+  isMobile.classList.remove("-translate-y-0", "opacity-100");
+  isMobile.classList.add("-translate-y-5", "opacity-0");
+  setTimeout(() => {
+    isMobile.classList.add("hidden");
+  }, 300);
+};
